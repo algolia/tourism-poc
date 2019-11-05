3baddd43b61c6c3812650909c9ed78576f6b8d5f
l0aded <oliverhanw@gmail.com>
2019-11-05 10:48:59 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
+import './App.css';
+import { appID, API_KEY } from './config'
+
+
+const searchClient = algoliasearch(
+  appID,
+  API_KEY
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <h1 className="header-title">
+            <a href="/">Tourism</a>
+          </h1>
+          <p className="header-subtitle">
+            using{' '}
+            <a href="https://github.com/algolia/react-instantsearch">
+              React InstantSearch
+            </a>
+          </p>
+        </header>
+
+        <div className="container">
+          <InstantSearch searchClient={searchClient} indexName="airbnb">
+            <div className="search-panel">
+              <div className="search-panel__results">
+                <SearchBox
+                  className="searchbox"
+                  translations={{
+                    placeholder: '',
+                  }}
+                />
+                <Hits hitComponent={Hit} />
+
+                <div className="pagination">
+                  <Pagination />
+                </div>
+              </div>
+            </div>
+          </InstantSearch>
+        </div>
+      </div>
+    );
+  }
+}
+
+function Hit(props) {
+  return (
+    <article>
+      <p>
+        <code>{JSON.stringify(props.hit).slice(0, 100)}...</code>
+      </p>
+    </article>
+  );
+}
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,7 @@
+var appID = 'latency'
+var API_KEY = '6be0576ff61c053d5f9a3225e2a90f76'
+
+module.exports = {
+  appID,
+  API_KEY
+}
