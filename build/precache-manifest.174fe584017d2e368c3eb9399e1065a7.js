c6dc86edd627ca3ae9a0ea94043274e688aecaa5
l0aded <oliverhanw@gmail.com>
2020-02-25 16:39:07 -0800

    build

---
+++ build/precache-manifest.174fe584017d2e368c3eb9399e1065a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "806e5039af670eafa97b",
+    "url": "/static/css/main.b057998e.chunk.css"
+  },
+  {
+    "revision": "806e5039af670eafa97b",
+    "url": "/static/js/main.806e5039.chunk.js"
+  },
+  {
+    "revision": "78abf5c2c3ee2065d3d8",
+    "url": "/static/js/1.78abf5c2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9ebf87bf286d0026dd3d6abd64f9797c",
+    "url": "/index.html"
+  }
+];
